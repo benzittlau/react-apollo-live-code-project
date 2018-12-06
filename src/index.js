5dd530cf07e6d47f6ab8b641cbdfa548edb50942
Ben Zittlau <ben@zittlau.ca>
2018-12-06 11:16:27 -0700

    Remove left behind apollo references in index.js.

--- src/index.js
+++ src/index.js
@@ -3,27 +3,12 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import { ApolloProvider } from 'react-apollo';
-import { ApolloClient } from 'apollo-client';
-import { createHttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
 import { BrowserRouter } from 'react-router-dom';
-
-const httpLink = createHttpLink({
-  uri: 'http://localhost:4000'
-})
-
-const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache()
-})
 
 
 ReactDOM.render(
   <BrowserRouter>
-    <ApolloProvider client={client}>
-      <App /> 
-    </ApolloProvider>
+    <App /> 
   </BrowserRouter>,
   document.getElementById('root')
 );
